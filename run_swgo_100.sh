4c5ceb70aecbe7cd7670b92dfe32784b80739300
Adan1210 <83679403+Adan1210@users.noreply.github.com>
2023-04-23 21:05:03 -0500

    Delete run_swgo_100.sh

--- run_swgo_100.sh
+++
@@ -1,8 +0,0 @@
-#enviroment variable pointing to the config folder use by HAWC sim
-export HAWCSIM_CONFIG=./config
-#Runnin the corsika shower through HAWC sim
-hawcsim-exe -i input_aerie_CORSIKA/100showers_CORSIKA/100showers_particle1_0_75_yanque_0_CORSIKA/DAT_100showers_particle1_0_75_yanque_0_CORSIKA -o ./output_aerie_XCDF/100showers_aerie/aerie_100showers_particle1_0_75_yanque_0.xcd --otype xcdf
-echo "Converting to root file format"
-xcdf-root --input ./output_aerie_XCDF/100showers_aerie/aerie_100showers_particle1_0_75_yanque_0.xcd -o ./output_aerie_XCDF/100showers_aerie/aerie_100showers_particle1_0_75_yanque_0.root
-echo "Done"
-
