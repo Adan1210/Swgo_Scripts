5a951766524248fdff2ecab2e55adfa10ea7b53e
Adan1210 <luis.twit@gmail.com>
2024-03-03 00:53:11 -0500

    cd

--- Read_data/read_data.jl
+++ Read_data/read_data.jl
@@ -1,13 +1,8 @@
 include("../data_analysis.jl")
 using .DataAnalysis: replace_ID_with_coords
-using  UnROOT, DataFrames, Base.Threads, CSV, Plots, Base.Filesystem
+using  UnROOT, DataFrames, Base.Threads, Plots, Base.Filesystem
 ##############################################################################################
-# Import the CSV file with the ID and Positions pf the PMT's.
 path_SWGO = dirname(pwd())
-path_ID_CSV = path_SWGO * "/Swgo_Scripts/Arrays/table_ID_and_positions_A1.csv";
-
-df_ID = CSV.read(path_ID_CSV, DataFrame);
-dict_ID = Dict(row.ID => (row.x, row.y, row.z) for row in eachrow(df_ID));
 ##############################################################################################
 # Files doesn't work
 path_filter = path_SWGO * "/swgo-aerie/missing_files.txt";  # Reemplaza esto con la ruta real
@@ -21,7 +16,80 @@ end
 ###########################################################################################
 # Initialice the names of all ROOT Files to work.
 path = [];
-list_files_values = [["DAT" * lpad(i, 6, '0'), j] for i in 1:10, j in 0:4 if !((i, j) in excluded_pairs)]; #aun no compilo esto, se quedo en 517
+list_files_values = [["DAT" * lpad(i, 6, '0'), j] for i in 1:2, j in 0:4 if !((i, j) in excluded_pairs)]; 
+###########################################################################################
+#Create the main_list, that list contain the data for work and have the form:
+list_positions_rᵢ = [];
+#Initialize the ROOT file and almacenated in the main_list.
+for i in eachindex(list_files_values)
+    # Create the names DAT000001, DAT000002, ...
+    DATXXX = list_files_values[i][1] 
+    YYY    = list_files_values[i][2]
+
+    # Initialize the path
+    path = path_SWGO * "/swgo_files/ROOT_Aerie_C/hawcsim-$(DATXXX)_A1_gamma_$(YYY)_50000.root"
+    
+    # Initialize the ROOT file
+    f = ROOTFile(path)
+    mytree = LazyTree(f ,"XCDF",["HAWCSim.Evt.X","HAWCSim.Evt.Y"])
+    main_list = [];
+
+    Threads.@threads for Tleaf in mytree 
+        Tbrunch = hcat(Tleaf[1], Tleaf[2])
+        push!(main_list, Tbrunch)
+    end
+    main_list_1 = []
+    push!(main_list_1, main_list)
+    push!(list_positions_rᵢ, main_list_1)
+end
+list_positions_rᵢ
+###########################################################################################
+using Optim
+
+function Sᵢ(A, σ, x, xᵢ, N, Rₘ)
+    return A / (2 * π * σ^2) * exp(-((x - xᵢ)^2 / (2 * σ^2))) + N / ((0.5 + abs(x - xᵢ) / Rₘ)^3)
+end
+
+Rₘ=120
+N=5*10^-5
+σ=10
+xᵢ=2
+
+# Definimos la función objetivo, que depende solo de las variables que queremos optimizar
+function objective_function(params)
+    A, x = params
+    Sᵢ(A, σ, x, xᵢ, N, Rₘ)
+end
+
+# Establecemos un punto de partida para la optimización
+initial_guess = [1.0, 1.0];
+
+# Realizamos la optimización utilizando el descenso de gradiente
+result = optimize(objective_function, initial_guess, GradientDescent());
+
+# Obtenemos los valores que minimizan la función
+optimal_params = Optim.minimizer(result);
+
+println("El valor óptimo de x_core es: ", optimal_params[1])
+println("El valor óptimo de σ es: ", optimal_params[2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ###########################################################################################
 #Create the main_list, that list contain the data for work and have the form:
 main_list = [];
@@ -31,4 +99,29 @@ f = ROOTFile(path)
 # Give the names of variables of the files
 names1 = names(LazyTree(f, "XCDF"))
 
-names1("Evt_X")
+mytree = LazyTree(f ,"XCDF",["HAWCSim.Evt.X","HAWCSim.Evt.Y"])
+
+
+list_energies_ID = []
+Threads.@threads for Tleaf in mytree 
+    Tbrunch = hcat(Tleaf[1], Tleaf[2])
+    push!(list_energies_ID, Tbrunch)
+end
+
+list_energies_ID
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
