fbd3500154ec665fa053697a41d2a3f88243d4f9
Adan1210 <luis.twit@gmail.com>
2024-03-09 14:07:58 -0500

    XD

--- Plots/plot_rec_energy.jl
+++ Plots/plot_rec_energy.jl
@@ -23,13 +23,11 @@ for i in eachindex(list_files_values)
         main_list = [];
 
         for Tleaf in mytree
-            if 10^4 > Tleaf[2] > 10^3 #Energy > 100 GeV
+            if 10^4 > Tleaf[2] > 10^3 && 300>=Tleaf[1]/100 && 300>=Tleaf[3]/100  #Energy > 100 GeV
                 element = [Tleaf[2], Tleaf[3]/100,Tleaf[1]/100]
                 push!(main_list, element)
             end
-
         end
-
         append!(list_positions_rᵢ, main_list)
     catch e
     end
@@ -38,12 +36,11 @@ end
 list_positions_rᵢ
 
 #######################################################################################
-using Plots
-using PlotUtils
-list_positions_rᵢ_2 = list_positions_rᵢ
-true_energy = [arr[1] for arr in list_positions_rᵢ_2]
-x = [arr[2] for arr in list_positions_rᵢ_2]
-y = [arr[3] for arr in list_positions_rᵢ_2]
+using Plots, PlotUtils
+
+true_energy = [arr[1] for arr in list_positions_rᵢ]
+x = [arr[2] for arr in list_positions_rᵢ]
+y = [arr[3] for arr in list_positions_rᵢ]
 
 # Crear el gráfico de dispersión con barra de energía de color
 scatter(x, y, 
@@ -73,39 +70,6 @@ scatter(x, y,
 # Save the figure
 file_name = path_SWGO*"/swgo_files/Plots/position_core1.png"
 savefig(file_name)
-
-
-###########################################################################################
-#Create the main_list, that list contain the data for work and have the form:
-list_positions_rᵢ = [];
-#Initialize the ROOT file and almacenated in the main_list.
-for i in eachindex(list_files_values)
-    # Create the names DAT000001, DAT000002, ...
-    DATXXX = list_files_values[i][1]
-    YYY    = list_files_values[i][2]
-
-    # Initialize the path
-    path = path_SWGO * "/swgo_files/ROOT_rec_Aerie_C/reco-$(DATXXX)_A1_gamma_$(YYY)_50000.root"
-    # Initialize the ROOT file
-    try
-        f = ROOTFile(path)
-        mytree = LazyTree(f ,"XCDF",["SimEvent.energyTrue","mc.delCore"])
-        main_list = [];
-
-        for Tleaf in mytree
-            if 10^4 > Tleaf[2] > 10^3 &&  300>=Tleaf[1]/100
-                element = [Tleaf[2], Tleaf[1]/100]
-                push!(main_list, element)
-            end
-
-        end
-
-        append!(list_positions_rᵢ, main_list)
-    catch e
-    end
-end
-
-list_positions_rᵢ
 
 #######################################################################################
 #Create the main_list, that list contain the data for work and have the form:
@@ -137,6 +101,7 @@ for i in eachindex(list_files_values)
     end
 end
 
+list_positions
 list_positions_rᵢ = [];
 list_energies = [];
 
@@ -151,48 +116,44 @@ list_energies
 
 mean_r = [];
 std_r = [];
+energy_list = [];
 for j in 10^4:10^4:9*10^4
     positions_j = [i for (i, x) in enumerate(list_energies) if j <= x <= j+10^4]
     list_positions_rᵢ_j = [list_positions_rᵢ[i] for i in positions_j]
     push!(mean_r, mean(list_positions_rᵢ_j))
     push!(std_r, std(list_positions_rᵢ_j))
+    push!(energy_list, j)
 end
 
 mean_r
 std_r
-
-
-
+energy_list
 
 #######################################################################################
-
 using Plots
 
-true_energy = [arr[1] for arr in list_positions_rᵢ]
-r = [arr[2] for arr in list_positions_rᵢ]
-
-# Crear el gráfico de dispersión con barra de energía de color
-scatter(true_energy, r, 
-    xlabel="Energy (GeV)",                                        #label of x axis
-    ylabel="r (m)",                                        #label of y axis
-    xscale=:log10,
-    xlim = (10^1.2,10^6.2),
-    ylim = ( 0, 30),
-    title="Diference core location vs initial particle energy (GeV)", #title
-    label=:"Diference between inicial position vs reconstructed position",                                      #label
-    legend=:topright,                                       #lengend
-    framestyle=:box, gridstyle=:solid,                      #frame
-    gridlinewidth=2, gridalpha=0.3,                         #grid and width
-
-
-    linestyle=:solid, linewidth=1,                          #type of line
-    marker=:circle, markersize=2,                           #marker and size
-    markerstrokecolor=:black, markerstrokewidth=0.2,        #contour of the marker
+bar(
+    energy_list, mean_r, yerr = std_r,
+    ylim=(0,15),                     #limits of y axis
+    xticks=[10^4*c for c in (1:1:9)], #limits of x axis
+    yticks=(0:2.5:15),
+    title="Mean of Core Position",                            #title
+    label=:"Photon",                                                       #label
+    legendtitle="Primary Particle:", legend =:topright,                    #lengend
+    framestyle=:box, gridstyle=:solid,                                     #frame
+    gridlinewidth=2, gridalpha=0.3,                                        #grid and width
+    xlabel="Primary Energy (GeV)",                                         #label of x axis
+    ylabel="r (m)",                                            #\label of y axis
+    linestyle=:solid, linewidth=1.5,                                       #type of line
+    marker=:circle, markercolor=:red, markersize=10,                        #marker and size
+    markerstrokecolor=:black, markerstrokewidth=0.5,                       #contour of the marker
     titlefont=font("Times New Roman",10),
     guidefont=font("Times New Roman",10),
-
+    legendfont=font("Times New Roman",10),
     dpi=500)
+
 
 # Save the figure
 file_name = path_SWGO*"/swgo_files/Plots/position_core3.png"
 savefig(file_name)
+
